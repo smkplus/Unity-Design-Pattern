c6b4ab312aed07444c25c529d9cd1ae59079bf7d
Seyed Morteza Kamali <smkplus3d@gmail.com>
2019-10-27 08:23:10 +0330

    Add Another way to Implement Command Pattern

---
+++ Unity-Design-Pattern/Assets/Scripts/Command Pattern/MemorySimon/Box.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Box : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip audioClip;
+
+    private Material _material;
+
+    private bool _isTurnOn;
+
+    private void Awake()
+    {
+        _material = GetComponent<MeshRenderer>().material;
+    }
+
+
+    private void OnMouseDown()
+    {
+        if(_isTurnOn) return;
+        TurnOn();
+        MemorySimon.Instance.actions.Add(TurnOn);
+
+    }
+
+    public void PlaySound()
+    {
+        AudioSource.PlayClipAtPoint(audioClip,transform.position);
+
+    }
+
+    public void TurnOn()
+    {
+        _isTurnOn = true;
+        _material.color *= 2;
+        PlaySound();
+
+        Invoke(nameof(TurnOff),0.5f);
+    }
+    
+    public void TurnOff()
+    {
+        _isTurnOn = false;
+        _material.color /= 2;
+    }
+
+
+}
---
+++ Unity-Design-Pattern/Assets/Scripts/Command Pattern/MemorySimon/MemorySimon.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class MemorySimon : MonoBehaviour
+{
+   public static MemorySimon Instance;
+   public List<Action> actions = new List<Action>();
+
+   private void Start()
+   {
+      Instance = this;
+   }
+
+   private void Update()
+   {
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+         StartCoroutine(Replay());
+      }
+   }
+
+   private void OnGUI()
+   {
+      GUI.Label(new Rect(Screen.width/2f - 100, Screen.height/2f + 100, 400, 800), "Press Space To Replay");
+   }
+
+   public IEnumerator Replay()
+   {
+      foreach (var action in actions.ToList())
+      {
+         action.Invoke();
+         yield return new WaitForSeconds(0.5f);
+      }
+   }
+}
